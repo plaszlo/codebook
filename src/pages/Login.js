ba160d85ef0abe3b528ffa228ddea5c4edec5055
László Papp <pappko9@gmail.com>
2026-07-04 09:58:56 +0100

    login guest env

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -25,8 +25,8 @@ export const Login = () => {
   }
 
 async function handleLoginGuest(){
-  email.current.value = 'las@example.hu';
-  password.current.value = 'Miruci7';
+  email.current.value = process.env.REACT_APP_GUEST_LOGIN;
+  password.current.value = process.env.REACT_APP_GUEST_PASSWORD;
   try{
     const authDetail = {
       email: email.current.value,
